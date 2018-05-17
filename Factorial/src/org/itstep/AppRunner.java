a1481fe64d50198b46f6b0315d0e98547715cad8
hp <hp@172.31.2.15>
2018-05-17 20:12:00 +0300

    Commit

--- Factorial/src/org/itstep/AppRunner.java
+++ Factorial/src/org/itstep/AppRunner.java
@@ -3,9 +3,14 @@ package org.itstep;
 public class AppRunner {
 
 	public static void main(String[] args) {
+		int number = 15;
 		FactorialInit factorialInit = new FactorialInit();
-		int factorial = factorialInit.getFactorial(7);
+		int factorial = factorialInit.getFactorial(number);
+		if(number>=0 && number != 10) {
 		System.out.println(factorial);
+		}else {
+		System.out.println("Incorrect enter number!");
+		}
 
 	}
 
